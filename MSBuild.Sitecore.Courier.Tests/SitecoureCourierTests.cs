e967e1a0e7d75a592995c788303c4be4f103bf1b
Kevin Obee <kevin.obee@gmail.com>
2013-03-23 13:55:38 +0000

    MSBuild task added

---
+++ MSBuild.Sitecore.Courier.Tests/SitecoureCourierTests.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using MSBuild.Sitecore.Courier.Tasks;
+using Microsoft.Build.Framework;
+
+using Moq;
+using NUnit.Framework;
+
+namespace MSBuild.Sitecore.Courier.Tests
+{
+    [TestFixture]
+    public class SitecoureCourierTests
+    {
+        private string _outputUpdateFile;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _outputUpdateFile = @".\Test.update";
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+//            File.Delete(_outputUpdateFile);
+        }
+        
+        [Test]
+        public void SitecoreCourier_Execute_Returns_True()
+        {
+            // Arrange
+            var mockBuildEngine = new Mock<IBuildEngine>();
+            var task = new SitecoreCourier();
+            task.BuildEngine = mockBuildEngine.Object;
+            task.ToolPath = @"..\..\..\Sitecore.Courier.Runner\bin\Debug\";
+            task.SourceDirectory = @".";
+            task.TargetDirectory = @".";
+            task.OutputUpdateFile = _outputUpdateFile;
+
+            // Act
+            var result = task.Execute();
+
+            // Assert
+            Assert.That(result, Is.True, "Execute failed");
+        }
+    }
+}
---
+++ MSBuild.Sitecore.Courier/Tasks/SitecoreCourier.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using Microsoft.Build.Utilities;
+
+namespace MSBuild.Sitecore.Courier.Tasks
+{
+    public class SitecoreCourier : ToolTask
+    {
+        public string SourceDirectory { get; set; }
+        public string TargetDirectory { get; set; }
+        public string OutputUpdateFile { get; set; }
+
+        public SitecoreCourier()
+        {
+            ToolPath = "";
+        }
+
+        protected override string GenerateFullPathToTool()
+        {
+            return Path.Combine(ToolPath, ToolName);
+        }
+
+        protected override string ToolName
+        {
+            get
+            {
+                return @"Sitecore.Courier.Runner.exe";
+            }
+        }
+
+        protected override string GenerateCommandLineCommands()
+        {
+            var builder = new CommandLineBuilder();
+
+            builder.AppendSwitchIfNotNull("/source:", SourceDirectory);
+            builder.AppendSwitchIfNotNull("/target:", TargetDirectory);
+            builder.AppendSwitchIfNotNull("/output:", OutputUpdateFile);
+
+            return builder.ToString();
+        }
+    }
+}
